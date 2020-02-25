951ff8f2818f02a24cc7c79d7d5113664f9ecdeb
Ryan Boone <falldowngoboone@gmail.com>
2020-02-25 07:16:15 -0600

    Build out initial data

---
+++ src/_data/business.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'E.V. Boone DDS',
+  phone: '409-769-4433',
+  address: {
+    streetLevel1: '1090 N Main St',
+    streetLevel2: '',
+    locality: 'Vidor',
+    region: 'TX',
+    postalCode: '77662',
+  },
+};
